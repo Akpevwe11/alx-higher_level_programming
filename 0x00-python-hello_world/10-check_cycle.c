5765ef39bf086166487d4217684f7500d9a57506
praisex64 <essiakpevwepraise@gmail.com>
2023-11-27 18:43:24 -0800

    10-cycle_check

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,22 +9,22 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow_ptr, *fast_ptr;
+	listint_t *slow, *fast;
 
 	if (!list || !list->next)
 		return (0);
 
-	slow_ptr = list->next;
-	fast_ptr = list->next->next;
+	slow = list->next;
+	fast = list->next->next;
 
-	while (slow_ptr && fast_ptr && fast_ptr->next)
+	while (slow && fast && fast->next)
 	{
-		if (slow_ptr == fast_ptr)
+		if (slow == fast)
 			return (1);
 
 
-		slow_ptr = slow_ptr->next;
-		fast_ptr = fast_ptr->next->next;
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 	return (0);
 }
