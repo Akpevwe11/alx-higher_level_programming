470ac93b4ad2025937d0e6fd0836e677c6d3b070
praisex64 <essiakpevwepraise@gmail.com>
2023-12-03 14:30:49 -0800

    taskfx

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,26 +4,25 @@ def add_tuple(tuple_a=(), tuple_b=()):
     len_b = len(tuple_b)
 
     if len_a >= 2:
-        first_element_a = tuple_a[0]
-        second_element_a = tuple_a[1]
+        first_a = tuple_a[0]
+        second_a = tuple_a[1]
     elif len_a == 1:
-        first_element_a = tuple_a[0]
-        second_element_a = 0
+        first_a = tuple_a[0]
+        second_a = 0
     elif len_a == 0:
-        first_element_a = 0
-        second_element_a = 0
+        first_a = 0
+        second_a = 0
 
     if len_b >= 2:
-        first_element_b = tuple_b[0]
-        second_element_b = tuple_b[1]
+        first_b = tuple_b[0]
+        second_b = tuple_b[1]
     elif len_b == 1:
-        first_element_b = tuple_b[0]
-        second_element_b = 0
+        first_b = tuple_b[0]
+        second_b = 0
     elif len_b == 0:
-        first_element_b = 0
-        second_element_b = 0
+        first_b = 0
+        second_b = 0
 
-    result_tuple = (first_element_a + first_element_b,
-                   second_element_a + second_element_b)
-
+    result_tuple = (first_a + first_b,second_a + second_b)
+                
     return result_tuple
