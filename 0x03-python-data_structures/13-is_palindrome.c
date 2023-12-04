0bdc6a98434d5f53eae528993efa3556feb2bd95
praisex64 <essiakpevwepraise@gmail.com>
2023-12-04 07:02:50 -0800

    fx efficieny

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,32 +8,32 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current_node = *head;
-	int list_length, *element_array, i;
+	listint_t *temp_node = *head;
+	int list_length, *element_arr, i;
 
 	if (head == NULL)
 		return (1);
 
-	for (list_length = 0; current_node; list_length++)
-		current_node = current_node->next;
+	for (list_length = 0; temp_node; list_length++)
+		temp_node = temp_node->next;
 
-	element_array = malloc(sizeof(int) * list_length);
-	if (!element_array)
+	element_arr = malloc(sizeof(int) * list_length);
+	if (!element_arr)
 		exit(100);
 
-	current_node = *head;
-	for (i = 0; current_node; current_node = current_node->next, i++)
-		element_array[i] = current_node->n;
+	temp_node = *head;
+	for (i = 0; temp_node; temp_node = temp_node->next, i++)
+		element_arr[i] = temp_node->n;
 
 	for (i = 0; i < (list_length / 2); i++)
 	{
-		if (element_array[i] != element_array[list_length - 1 - i])
+		if (element_arr[i] != element_arr[list_length - 1 - i])
 		{
-			free(element_array);
+			free(element_arr);
 			return (0);
 		}
 	}
 
-	free(element_array);
+	free(element_arr);
 	return (1);
 }
