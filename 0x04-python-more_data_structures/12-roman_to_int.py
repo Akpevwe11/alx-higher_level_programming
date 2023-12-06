6bc5c0fd59c62f7669fc60dab076b0308cd195e1
praisex64 <essiakpevwepraise@gmail.com>
2023-12-06 09:16:36 -0800

    task 13

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total_val = 0
+    prev = 0
+
+    if type(roman_string) is str and roman_string:
+        for c in range(len(roman_string) - 1, -1, -1):
+            if roman_numerals[roman_string[c]] >= prev:
+                total_val += roman_numerals[roman_string[c]]
+            else:
+                total_val -= roman_numerals[roman_string[c]]
+            prev = roman_numerals[roman_string[c]]
+    return total_val
