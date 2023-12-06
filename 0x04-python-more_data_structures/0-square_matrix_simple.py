8ea909bf1b45f3914cc4b6c155abcebdc5bf5bd0
praisex64 <essiakpevwepraise@gmail.com>
2023-12-06 09:31:16 -0800

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[col ** 2 for col in row] for row in matrix]
+    if len(matrix) == 0:
+        return None
+    else:
+        matrix_sq = [[x ** 2 for x in row] for row in matrix]
+        return matrix_sq
