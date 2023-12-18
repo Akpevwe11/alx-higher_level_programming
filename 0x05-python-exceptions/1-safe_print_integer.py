78847374664a26ce3a9121219bbc26784f1a43b2
praisex64 <essiakpevwepraise@gmail.com>
2023-12-18 08:55:02 -0800

    task 2

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    int_val = False
+    try:
+        print("{:d}".format(value))
+        int_val = True
+    except Exception as e:
+        pass
+    return status
