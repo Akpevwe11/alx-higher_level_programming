b7465282bab810421d89c176933150e128417741
praisex64 <essiakpevwepraise@gmail.com>
2024-01-05 01:20:16 -0800

    removed a folder

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 
 /**
-* print_python_list_info - prints python list
+* print_python_list_info - prints python list information 
 * @p: list
 * Return: Void
 */
 
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size_p, allocated, idx = 0;
+	Py_ssize_t list_size, allocated, index = 0;
 	PyObject *element;
 
-	size_p = PyList_Size(p);
+	list_size = PyList_Size(p);
 	allocated = ((PyListObject *)p)->allocated;
-	printf("[*] Size of the Python List = %ld\n", size_p);
+	printf("[*] Size of the Python List = %ld\n", list_size);
 	printf("[*] Allocated = %ld\n", allocated);
-	while (idx < size_p)
+	while (index < list_size)
 	{
-		element = PyList_GET_ITEM(p, idx);
-		printf("Element %ld: %s\n", idx, element->ob_type->tp_name);
-		idx++;
+		element = PyList_GET_ITEM(p, index);
+		printf("Element %ld: %s\n", index, element->ob_type->tp_name);
+		index++;
 	}
 }	
